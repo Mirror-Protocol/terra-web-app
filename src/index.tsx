d3171c5d41499d3769bab013c59fcda27f9bf2ef
sim <sim@terra.money>
2020-08-31 19:48:54 +0900

    Init app

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { StrictMode } from "react"
+import { render } from "react-dom"
+import "./index.scss"
+import App from "./App"
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  </StrictMode>,
+  document.getElementById("root")
+)
