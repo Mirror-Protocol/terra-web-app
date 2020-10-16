3e8221eae3a15fd1f433fa8e69a3179e48362f6b
sim <sim@terra.money>
2020-10-16 17:39:26 +0900

    feature(Stake): Remove balance on stake details

--- src/pages/Gov/GovStake.tsx
+++ src/pages/Gov/GovStake.tsx
@@ -1,21 +1,17 @@
 import React from "react"
 import { MIR } from "../../constants"
-import { useWallet } from "../../hooks"
 import Page from "../../components/Page"
 import StakeForm from "../../forms/StakeForm"
 import useHash from "../useHash"
 import { Type } from "../Stake"
-import StakeDetailsContents from "../Stake/StakeDetailsContents"
 import { MenuKey } from "../Gov"
 
 const GovStake = () => {
-  const { address } = useWallet()
   const { hash: type } = useHash<Type>(Type.STAKE)
   const tab = { tabs: [Type.STAKE, Type.UNSTAKE], current: type }
 
   return (
     <Page title={MenuKey.STAKE}>
-      {address && <StakeDetailsContents symbol={MIR} gov />}
       {type && <StakeForm type={type} symbol={MIR} tab={tab} key={type} gov />}
     </Page>
   )
--- src/pages/Stake/StakeDetails.tsx
+++ src/pages/Stake/StakeDetails.tsx
@@ -1,22 +1,18 @@
 import React from "react"
 import { useParams } from "react-router-dom"
-import { useWallet } from "../../hooks"
 import Page from "../../components/Page"
 import StakeForm from "../../forms/StakeForm"
 import useHash from "../useHash"
 import { Type } from "../Stake"
 import StakeFormHeader from "./StakeFormHeader"
-import StakeDetailsContents from "./StakeDetailsContents"
 
 const StakeDetails = () => {
-  const { address } = useWallet()
   const { symbol } = useParams<{ symbol: string }>()
   const { hash: type } = useHash<Type>(Type.STAKE)
   const tab = { tabs: [Type.STAKE, Type.UNSTAKE], current: type }
 
   return (
     <Page title={<StakeFormHeader>{symbol}</StakeFormHeader>}>
-      {address && <StakeDetailsContents symbol={symbol} />}
       {type && <StakeForm type={type} symbol={symbol} tab={tab} key={type} />}
     </Page>
   )
