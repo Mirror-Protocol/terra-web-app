7e7a16769b8613882c525544f3e7f4302e2f197f
sim <sim@terra.money>
2020-11-24 15:56:04 +0900

    Revert "change: Retire moonshine"
    This reverts commit b2adcffb82b1c2c3495d73bbc25a600766c71995.

--- src/networks.ts
+++ src/networks.ts
@@ -5,6 +5,12 @@ const networks: Record<string, LocalNetworkConfig> = {
     stats: "https://tequila-graph.mirror.finance/graphql",
     fee: { gasPrice: 0.15, amount: 50000 }, // 0.050000 UST
   },
+  moonshine: {
+    contract: "https://whitelist.mirror.finance/moonshine.json",
+    mantle: "https://moonshine-mantle.terra.dev/",
+    stats: "https://moonshine-graph.mirror.finance/graphql",
+    fee: { gasPrice: 0.15, amount: 50000 }, // 0.050000 UST
+  },
 }
 
 export default networks
