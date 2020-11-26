710bd7cd4d3924dda9a8166dbbb07fe9c4648298
sim <sim@terra.money>
2020-11-27 01:55:20 +0900

    fix: Refactor LinkButton

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -2,7 +2,14 @@ import { Link, LinkProps as Props } from "react-router-dom"
 import { getAttrs } from "./Button"
 
 export type LinkProps = ButtonProps & Props
-const LinkButton = (props: LinkProps) =>
-  props.disabled ? <span {...getAttrs(props)} /> : <Link {...getAttrs(props)} />
+const LinkButton = (props: LinkProps) => {
+  const { children, ...attrs } = getAttrs(props)
+
+  return props.disabled ? (
+    <a {...attrs}>{children}</a>
+  ) : (
+    <Link {...attrs}>{children}</Link>
+  )
+}
 
 export default LinkButton
