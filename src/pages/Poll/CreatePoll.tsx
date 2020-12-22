95675879daac5ac5147961b855f91250852fe173
sim <sim@terra.money>
2020-12-22 15:48:47 +0900

    feature: Hide gov update poll for a while

--- src/pages/Poll/CreatePoll.tsx
+++ src/pages/Poll/CreatePoll.tsx
@@ -56,9 +56,12 @@ const CreatePoll = () => {
               <h1 className={styles.title}>{TITLE}</h1>
             </header>
 
-            {Object.entries(Buttons).map(([key, item]) => (
-              <CreatePollButton {...item} hash={key} key={key} />
-            ))}
+            {Object.entries(Buttons).map(
+              ([key, item]) =>
+                key !== Type.GOV_UPDATE && (
+                  <CreatePollButton {...item} hash={key} key={key} />
+                )
+            )}
           </Card>
         </Container>
       ) : (
