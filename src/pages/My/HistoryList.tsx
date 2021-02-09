dbc3eb73d26557fe3aad95bc9980cf1fbf9ee098
sim <sim@terra.money>
2021-02-09 20:54:05 +0900

    fix: Remove unused import

--- src/pages/My/HistoryList.tsx
+++ src/pages/My/HistoryList.tsx
@@ -1,6 +1,5 @@
 import Button from "../../components/Button"
 import Card from "../../components/Card"
-import { UUSD } from "../../constants"
 import useTxs from "../../statistics/useTxs"
 import DownloadCSV from "./DownloadCSV"
 import HistoryItem from "./HistoryItem"
