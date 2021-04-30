668eb9e32cbf3c7e2d4cde0ce177b4cc32abe061
sim <sim@terra.money>
2021-04-30 19:51:29 +0900

    wip: 익스텐션 설치 버튼 출력

--- src/layouts/ConnectList.tsx
+++ src/layouts/ConnectList.tsx
@@ -1,4 +1,6 @@
+import { ReactNode } from "react"
 import { useWallet, ConnectType } from "@terra-money/wallet-provider"
+import { useInstallChromeExtension } from "@terra-money/wallet-provider"
 import { ReactComponent as Terra } from "../images/Terra.svg"
 import WalletConnect from "../images/WalletConnect.png"
 import styles from "./ConnectList.module.scss"
@@ -7,34 +9,48 @@ const size = { width: 24, height: 24 }
 
 const ConnectList = () => {
   const { availableConnectTypes, connect } = useWallet()
+  const installChromeExtension = useInstallChromeExtension()
 
-  const buttons = {
-    [ConnectType.CHROME_EXTENSION]: {
-      label: "Terra Station (extension)",
-      image: <Terra {...size} />,
-    },
-    [ConnectType.WALLETCONNECT]: {
-      label: "Terra Station (mobile)",
-      image: <img src={WalletConnect} {...size} alt="WalletConnect" />,
-    },
-  }
+  type Button = { label: string; image: ReactNode; onClick: () => void }
+  const buttons = ([] as Button[])
+    .concat(
+      installChromeExtension
+        ? {
+            label: "Terra Station (extension)",
+            image: <Terra {...size} />,
+            onClick: installChromeExtension,
+          }
+        : []
+    )
+    .concat(
+      availableConnectTypes.includes(ConnectType.CHROME_EXTENSION)
+        ? {
+            label: "Terra Station (extension)",
+            image: <Terra {...size} />,
+            onClick: () => connect(ConnectType.CHROME_EXTENSION),
+          }
+        : []
+    )
+    .concat(
+      availableConnectTypes.includes(ConnectType.WALLETCONNECT)
+        ? {
+            label: "Terra Station (mobile)",
+            image: <img src={WalletConnect} {...size} alt="WalletConnect" />,
+            onClick: () => connect(ConnectType.WALLETCONNECT),
+          }
+        : []
+    )
 
   return (
     <article className={styles.component}>
       <h1 className={styles.title}>Connect to a wallet</h1>
       <section>
-        {Object.entries(buttons)
-          .filter(([key]) => availableConnectTypes.includes(key as ConnectType))
-          .map(([key, { label, image }]) => (
-            <button
-              className={styles.button}
-              onClick={() => connect(key as ConnectType)}
-              key={key}
-            >
-              {label}
-              {image}
-            </button>
-          ))}
+        {Object.entries(buttons).map(([key, { label, image, onClick }]) => (
+          <button className={styles.button} onClick={onClick} key={key}>
+            {label}
+            {image}
+          </button>
+        ))}
       </section>
     </article>
   )
