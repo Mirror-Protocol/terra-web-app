ad816564e3bb532fd3f0badbb5db9d7d1ea51886
sim <sim@terra.money>
2021-06-23 01:41:59 +0900

    fix: Filter locked UST by mint positions

--- src/data/my/locked.ts
+++ src/data/my/locked.ts
@@ -12,16 +12,18 @@ export const useMyLockedUST = () => {
     ({ idx: a }, { idx: b }) => number(minus(b, a))
   )
 
-  const dataSource = values.map((item) => {
-    const position = mintPositions.find(({ idx }) => idx === item.idx)!
-    const { token } = parseAssetInfo(position.asset.info)
-    const { locked_amount, unlock_time } = item
-    const isUnlocked = unlock_time * 1000 < Date.now()
-    const locked = isUnlocked ? "0" : locked_amount
-    const unlocked = isUnlocked ? locked_amount : "0"
+  const dataSource = values
+    .filter(({ idx }) => mintPositions.some((item) => idx === item.idx))
+    .map((item) => {
+      const position = mintPositions.find(({ idx }) => idx === item.idx)
+      const { token } = parseAssetInfo(position!.asset.info)
+      const { locked_amount, unlock_time } = item
+      const isUnlocked = unlock_time * 1000 < Date.now()
+      const locked = isUnlocked ? "0" : locked_amount
+      const unlocked = isUnlocked ? locked_amount : "0"
 
-    return { ...item, ...whitelist[token], locked, unlocked }
-  })
+      return { ...item, ...whitelist[token], locked, unlocked }
+    })
 
   const totalLockedUST = sum(dataSource.map(({ locked }) => locked))
   const totalUnlockedUST = sum(dataSource.map(({ unlocked }) => unlocked))
