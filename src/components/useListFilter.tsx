f271a4758d452994351435564c49bbd44a3f1ecb
sim <sim@terra.money>
2021-06-24 02:55:04 +0900

    design: Split selector component with align props

--- src/components/useListFilter.tsx
+++ src/components/useListFilter.tsx
@@ -24,6 +24,7 @@ const useListFilter = (initial: string, sorters: Dictionary<Sorter>) => {
           value: key,
           children: label,
         }))}
+        right
       />
     </Search>
   )
