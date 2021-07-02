f926a6e4375319665f3cc2a7fb2059fe58731d2a
maro <honeymaro@live.co.kr>
2021-07-03 00:54:22 +0900

    feat: adjust font-size of input of SwapFormGroup

--- src/forms/SwapFormGroup.tsx
+++ src/forms/SwapFormGroup.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 import classNames from "classnames/bind"
 import styles from "./SwapFormGroup.module.scss"
@@ -38,6 +38,27 @@ const SwapFormGroup = ({
     outline: true,
     children: "Max",
   }
+  const inputRef = useRef<HTMLInputElement | null>()
+  const [inputFontSize, setInputFontSize] = useState(24)
+  useEffect(() => {
+    if (inputRef?.current) {
+      const computedStyle = window.getComputedStyle(inputRef.current)
+      const elSpan = document.createElement("span")
+      elSpan.innerText = inputRef?.current?.value
+      elSpan.style.visibility = "hidden"
+      elSpan.style.position = "absolute"
+      elSpan.style.left = "-9999px"
+      elSpan.style.font = computedStyle.font
+      elSpan.style.fontSize = "24px"
+      document.body.appendChild(elSpan)
+      const calculatedFontSize =
+        (24 * inputRef.current.clientWidth) / elSpan.clientWidth
+      document.body.removeChild(elSpan)
+      setInputFontSize(calculatedFontSize > 24 ? 24 : calculatedFontSize)
+      return
+    }
+    setInputFontSize(24)
+  }, [input])
 
   return (
     <div className={styles.component}>
@@ -61,7 +82,14 @@ const SwapFormGroup = ({
           <section className={styles.wrapper}>
             <section className={styles.field}>
               {input ? (
-                <input {...input} />
+                <input
+                  {...input}
+                  ref={(e) => {
+                    input?.ref && input.ref(e)
+                    inputRef.current = e
+                  }}
+                  style={{ fontSize: inputFontSize }}
+                />
               ) : textarea ? (
                 <textarea {...textarea} />
               ) : select ? (
