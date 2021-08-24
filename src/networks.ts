9e0a3b48a3fbc4742cfb49f5adbfd920321e6cb7
sim <sim@terra.money>
2021-08-24 20:30:45 +0900

    Increase gas price

--- src/networks.ts
+++ src/networks.ts
@@ -14,7 +14,7 @@ const networks: Record<string, MirrorNetworkInfo> = {
       ethereum: "terra13yxhrk08qvdf5zdc9ss5mwsg5sf7zva9xrgwgc",
       bsc: "terra1g6llg3zed35nd3mh9zx6n64tfw3z67w2c48tn2",
     },
-    fee: { gasPrice: 0.38, amount: 250000 }, // 0.25 UST
+    fee: { gasPrice: 0.456, amount: 300000 }, // 0.3 UST
   },
   testnet: {
     name: "testnet",
