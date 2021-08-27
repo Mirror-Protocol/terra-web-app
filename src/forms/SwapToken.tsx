a0a44be835d9f6df5ebfc1a8f2b1e3324cc1bec4
JoowonYun <joowon@delightlabs.io>
2021-08-27 16:20:46 +0900

    feat: show contract address in select token

--- src/forms/SwapToken.tsx
+++ src/forms/SwapToken.tsx
@@ -3,43 +3,50 @@ import { gt } from "../libs/math"
 import { format, lookupSymbol } from "../libs/parse"
 import styles from "./SwapToken.module.scss"
 import { GetTokenSvg } from "../helpers/token"
+import { truncate } from "libs/text"
 
 interface Props extends AssetItem {
+  contract_addr?: string
   formatTokenName?: (symbol: string) => string
 }
 
-const SwapToken = ({ symbol, balance, formatTokenName }: Props) => {
+const SwapToken = ({ symbol, balance, contract_addr,  formatTokenName }: Props) => {
   const symbols = symbol.split("-")
   return (
     <article className={styles.asset}>
       <header className={styles.header}>
-        <div className={styles.symbol}>
-          <div className={styles.logo}>
-            <img src={GetTokenSvg(symbols[0])} width={25} height={25} alt="" />
-          </div>
-          <div className={styles.name}>
-            {formatTokenName?.(symbols[0]) ?? lookupSymbol(symbols[0])}
-          </div>
-          {symbols.length > 1 ? (
-            <div className={styles.divide}>
-              <span>-</span>
-            </div>
-          ) : undefined}
-          {symbols.length > 1 ? (
+        <div className={styles.symbol_name}>
+          <div className={styles.symbol}>
             <div className={styles.logo}>
-              <img
-                src={GetTokenSvg(symbols[1])}
-                width={25}
-                height={25}
-                alt=""
-              />
+              <img src={GetTokenSvg(symbols[0])} width={25} height={25} alt="" />
             </div>
-          ) : undefined}
-          {symbols.length > 1 ? (
             <div className={styles.name}>
-              {formatTokenName?.(symbols[1]) ?? lookupSymbol(symbols[1])}
+              {formatTokenName?.(symbols[0]) ?? lookupSymbol(symbols[0])}
             </div>
-          ) : undefined}
+            {symbols.length > 1 ? (
+              <div className={styles.divide}>
+                <span>-</span>
+              </div>
+            ) : undefined}
+            {symbols.length > 1 ? (
+              <div className={styles.logo}>
+                <img
+                  src={GetTokenSvg(symbols[1])}
+                  width={25}
+                  height={25}
+                  alt=""
+                />
+              </div>
+            ) : undefined}
+            {symbols.length > 1 ? (
+              <div className={styles.name}>
+                {formatTokenName?.(symbols[1]) ?? lookupSymbol(symbols[1])}
+              </div>
+            ) : undefined}
+            </div>
+            <div className={styles.address}>
+              {truncate(contract_addr, [8, 8])}
+            </div>
         </div>
       </header>
 
