030a5c6d1531e554bd70776c48009781a6c160ff
JoowonYun <joowon@delightlabs.io>
2021-10-17 20:16:30 +0900

    fix: default gas

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -17,7 +17,7 @@ const networks: Record<string, MirrorNetworkInfo> = {
     swap: "/swap.json",
     mantle: "https://fcd.terra.dev/",
     stats: "https://fcd.terra.dev/",
-    fee: { gasPrice: "0.00506", amount: "1518", gas: "800000" }, // 0.000500 UST
+    fee: { gasPrice: "0.00506", amount: "1518", gas: "1000000" }, // 0.000500 UST
     factory: "terra1ulgw0td86nvs4wtpsc80thv6xelk76ut7a7apj",
     service:
       process.env.REACT_APP_MAINNET_SERVICE_URL || "https://api.terraswap.io/",
@@ -27,12 +27,12 @@ const networks: Record<string, MirrorNetworkInfo> = {
     chainID: "bombay-12",
     lcd: "https://bombay-lcd.terra.dev",
     fcd: "https://bombay-fcd.terra.dev",
-    id: "bombay-11",
+    id: "bombay-12",
     contract: "/tequila.json",
     swap: "/swap.json",
     mantle: "https://bombay-mantle.terra.dev/",
     stats: "https://bombay-fcd.terra.dev/",
-    fee: { gasPrice: "0.15", amount: "50000", gas: "450000" }, // 0.050000 UST
+    fee: { gasPrice: "0.00506", amount: "1518", gas: "1000000" }, // 0.050000 UST
     factory: "terra18qpjm4zkvqnpjpw0zn0tdr8gdzvt8au35v45xf",
     service:
       process.env.REACT_APP_TESTNET_SERVICE_URL ||
