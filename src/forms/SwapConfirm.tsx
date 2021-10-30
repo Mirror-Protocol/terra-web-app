e4ca4026b707d29a86c6f540d29082fdd65d02d7
maro <honeymaro@live.co.kr>
2021-10-30 22:29:00 +0900

    update: SwapComfirm

--- src/forms/SwapConfirm.tsx
+++ src/forms/SwapConfirm.tsx
@@ -3,14 +3,16 @@ import styles from "./SwapConfirm.module.scss"
 
 const SwapConfirm = ({ list }: { list: Content[] }) => (
   <ul className={styles.list}>
-    {list.map(({ title, content }, index) => (
-      <li className={styles.item} key={index}>
-        <article className={styles.article}>
-          <h1 className={styles.title}>{title}</h1>
-          <p className={styles.content}>{content}</p>
-        </article>
-      </li>
-    ))}
+    {list
+      .map((item, key) => ({ ...item, key: `${key}` }))
+      .map(({ title, content, key }) => (
+        <li className={styles.item} key={key}>
+          <article className={styles.article}>
+            <h1 className={styles.title}>{title}</h1>
+            <p className={styles.content}>{content}</p>
+          </article>
+        </li>
+      ))}
   </ul>
 )
 
