0facd61796954a1d656653a883547ceee0dcb071
JoowonYun <joowon@delightlabs.io>
2022-01-02 22:36:50 +0900

    feat: support to tax

--- src/forms/SwapForm.tsx
+++ src/forms/SwapForm.tsx
@@ -276,6 +276,8 @@ const SwapForm = ({ type, tabs }: { type: Type; tabs: TabViewProps }) => {
     balance1
   )
 
+  const [tax, setTax] = useState<Coins>(new Coins())
+
   const simulationContents = useMemo(() => {
     if (
       !(
@@ -295,6 +297,8 @@ const SwapForm = ({ type, tabs }: { type: Type; tabs: TabViewProps }) => {
           decimals: tokenInfo1?.decimals,
         })
       : "0"
+
+    const taxs = tax.filter((coin) => !coin.amount.equals(0))
 
     return [
       ...insertIf(type === Type.SWAP, {
@@ -367,6 +371,21 @@ const SwapForm = ({ type, tabs }: { type: Type; tabs: TabViewProps }) => {
           </Count>
         ),
       },
+      ...insertIf(taxs.toArray().length > 0, {
+        title: `Tax`,
+        content: taxs.toArray().map((coin, index) => {
+          return index === 0 ? (
+            <Count symbol={coin.denom}>{lookup(coin.amount.toString())}</Count>
+          ) : (
+            <div>
+              <span>, </span>
+              <Count symbol={coin.denom}>
+                {lookup(coin.amount.toString())}
+              </Count>
+            </div>
+          )
+        }),
+      }),
       ...insertIf(type === Type.SWAP && profitableQuery?.tokenRoutes?.length, {
         title: (
           <TooltipIcon content="Optimized route for your optimal gain">
@@ -395,6 +414,7 @@ const SwapForm = ({ type, tabs }: { type: Type; tabs: TabViewProps }) => {
     type,
     lpContract,
     tokenInfo1,
+    tax,
   ])
 
   const getMsgs = useCallback(
@@ -447,11 +467,23 @@ const SwapForm = ({ type, tabs }: { type: Type; tabs: TabViewProps }) => {
     [isNativeToken]
   )
 
-  const [tax, setTax] = useState<Coins>(new Coins())
   const { gasPrice } = useGasPrice(formData[Key.feeSymbol])
   const getTax = useCallback(
-    async ({ value1, value2, max1, max2, token1, token2 }) => {
+    async ({ value1, value2, token1, token2 }) => {
       let newTax = tax
+
+      newTax.map((coin) => {
+        if (
+          !(
+            coin.denom === token1 ||
+            (type === Type.PROVIDE && coin.denom === token2)
+          )
+        ) {
+          newTax.set(coin.denom, 0)
+        }
+
+        return true
+      })
 
       const taxCap1 = await loadTaxInfo(token1)
       const taxCap2 = await loadTaxInfo(token2)
@@ -460,11 +492,9 @@ const SwapForm = ({ type, tabs }: { type: Type; tabs: TabViewProps }) => {
         token1 &&
         hasTaxToken(token1) &&
         (taxCap1 || taxCap1 === "") &&
-        taxRate &&
-        max1
+        taxRate
       ) {
         const tax1 = calcTax(toAmount(value1), taxCap1, taxRate)
-
         newTax.set(token1, tax1)
       }
       if (
@@ -472,15 +502,14 @@ const SwapForm = ({ type, tabs }: { type: Type; tabs: TabViewProps }) => {
         token2 &&
         hasTaxToken(token2) &&
         (taxCap2 || taxCap2 === "") &&
-        taxRate &&
-        max2
+        taxRate
       ) {
         const tax2 = calcTax(toAmount(value2), taxCap2, taxRate)
         newTax.set(token2, tax2)
       }
       return newTax
     },
-    [type, tax, loadTaxInfo, loadTaxRate]
+    [type, loadTaxInfo, loadTaxRate, tax]
   )
 
   const isTaxCalculating = useRef<boolean>(false)
@@ -506,7 +535,7 @@ const SwapForm = ({ type, tabs }: { type: Type; tabs: TabViewProps }) => {
       .finally(() => {
         isTaxCalculating.current = false
       })
-  }, [formData, getTax, tax])
+  }, [formData, tax, getTax])
 
   const validateForm = async (
     key:
--- src/libs/parse.ts
+++ src/libs/parse.ts
@@ -1,6 +1,39 @@
 import BigNumber from "bignumber.js"
 import numeral from "numeral"
-import { UST, UUSD } from "constants/constants"
+import {
+  AUT,
+  CAT,
+  CHT,
+  CNT,
+  EUT,
+  GBT,
+  HKT,
+  INT,
+  JPT,
+  KRT,
+  LUNA,
+  MNT,
+  SDT,
+  SGT,
+  THT,
+  UAUD,
+  UCAD,
+  UCHF,
+  UCNY,
+  UEUR,
+  UGBP,
+  UHKD,
+  UINR,
+  UJPY,
+  UKRW,
+  ULUNA,
+  UMNT,
+  USDR,
+  USGD,
+  UST,
+  UTHB,
+  UUSD,
+} from "constants/constants"
 import { tokenInfos } from "rest/usePairs"
 import { placeholder } from "forms/formHelpers"
 
@@ -68,8 +101,44 @@ export const lookup: Formatter = (amount = "0", contract_addr, config) => {
     .toString()
 }
 
-export const lookupSymbol = (symbol?: string) =>
-  symbol === UUSD ? UST : symbol
+export const lookupSymbol = (symbol?: string) => {
+  switch (symbol) {
+    case ULUNA:
+      return LUNA
+    case UKRW:
+      return KRT
+    case USDR:
+      return SDT
+    case UMNT:
+      return MNT
+    case UUSD:
+      return UST
+    case UAUD:
+      return AUT
+    case UCAD:
+      return CAT
+    case UCHF:
+      return CHT
+    case UCNY:
+      return CNT
+    case UEUR:
+      return EUT
+    case UGBP:
+      return GBT
+    case UHKD:
+      return HKT
+    case UINR:
+      return INT
+    case UJPY:
+      return JPT
+    case USGD:
+      return SGT
+    case UTHB:
+      return THT
+    default:
+      return symbol
+  }
+}
 
 export const format: Formatter = (amount, contract_addr, config) => {
   const value = new BigNumber(lookup(amount, contract_addr, config))
